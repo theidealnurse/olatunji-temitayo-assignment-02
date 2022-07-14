78948c480290c90f962f6e6b86f0b300d02b941c
Temitayo Olatunji <>
2022-07-14 23:21:58 +0000

    Challenges 1 and 2

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,14 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let sum = 0;
+for ( let i = 0, x = arrayOfNumbers.length; i<x;  i++){
+   sum+= arrayOfNumbers[i];
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+return sum;
+return arrayOfNumbers.length;
+}
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,8 +33,16 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+   let evenNumbers = 0;
+   for ( let i  = 0; i< arrayOfNumbers.length; i++){
+if ( arrayOfNumbers[i] % 2 ===0){
+evenNumbers++;
 }
+}
+return evenNumbers;
+return arrayOfNumbers.length;
+}
+assignment.countEvenNumbers = countEvenNumbers;
 // assignment.countEvenNumbers = countEvenNumbers;
 
 /**
